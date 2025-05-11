62c8e399ad21bd5c91bafd6d47dbe28882c1477e
UKay0 <34538592+UKay0@users.noreply.github.com>
2025-05-11 22:38:52 +0500

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try{
-            const response = await axios.post(`${import.meta.env.API_URL}/api/auth/login`, {
+            const response = await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
                 email,
                 password
             });
